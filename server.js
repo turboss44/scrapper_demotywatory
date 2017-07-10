94ab41e095e1b5cee26cef41f584f4cd07d5c578
turboss44 <turboss44@gmail.com>
2017-07-10 18:57:39 +0200

    new

--- server.js
+++ server.js
@@ -3,29 +3,27 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
-
 var img=[];
-var  url='http://demotywatory.pl';
-    request(url,function(error, response, html){
-      if(!error){
-        var $ = cheerio.load(html);
-        $('.demot_pic').each(function(){
-          var data = $(this);
-          var one_img = data.children().first().children().first().attr('src')
-          img.push(one_img);
+var url='http://demotywatory.pl';
 
-        });
-        console.log(img);
-        for(var i = 0; i < img.length; i++){
-          if(img[i]!==undefined){
-            console.log('siemanko');
-          request(img[i]).pipe(fs.createWriteStream('demot'+(i+1)+'.jpg'));
+request(url,function(error, response, html){
+  if(!error){
+    var $ = cheerio.load(html);
+    $('.demot_pic').each(function(){
+      var data = $(this);
+      var one_img = data.children().first().children().first().attr('src')
+      img.push(one_img);
+    });
+    console.log(img);
+    for(var i = 0; i < img.length; i++){
+      if(img[i]!==undefined){
+        console.log('siemanko');
+        request(img[i]).pipe(fs.createWriteStream('demot'+(i+1)+'.jpg'));
       }
     }
+  }
+})
 
-    }
-    })
+app.listen('7676')
 
-    app.listen('7676')
-
-    console.log('Scrapping on 7676')
+console.log('Scrapping on 7676')
