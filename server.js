5c55635320ffa08f4ab27d4eeede4f7577624dec
turboss44 <turboss44@gmail.com>
2017-07-10 17:47:15 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+
+var img=[]
+  url='http://www.demotywatory.pl';
+    request(url,function(error, response, html){
+      if(!error){
+        var $ = cheerio.load(html);
+        $('.demot_pic').each(function(){
+          var data = $(this);
+          var one_img = data.children().first().children().first().attr('src')
+          img.push(one_img);
+
+        });
+        console.log(img);
+
+        for(var i = 0; i < img.length; i++){
+          request(img[i]).pipe(fs.createWriteStream('demot'+(i+1)+'.jpg'));
+      }
+
+    }
+    })
+
+    app.listen('7676')
+
+    console.log('Scrapping on 7676')
